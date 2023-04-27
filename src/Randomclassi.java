056e3a11b0042f0ddd86b447d626e15c5915f844
Hakan Yılmaz <hakanyilmazhy1987@gmail.com>
2023-04-27 16:43:47 +0300

    first commit

---
+++ src/Randomclassi.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Randomclassi {
+
+    public static void main(String[] args) {
+
+
+        Random rnd=new Random();
+
+        int sayi;
+
+        sayi=rnd.nextInt();
+        System.out.println("sayi :" +sayi);
+
+
+        int sayi1=rnd.nextInt(5);
+        System.out.println("sayi1 = " + sayi1);
+
+
+
+    }
+
+
+
+}
